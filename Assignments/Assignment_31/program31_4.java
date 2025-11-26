0c269be5b3a7a74155a8e78ad0155d7d26de14b9
DikshaVispute <dikshavispute809@gmail.com>
2025-11-26 15:21:34 +0530

    Add Assignment31 solution Q1-Q5

---
+++ Assignments/Assignment_31/program31_4.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+/*
+    Input : iRow = 6    iCol = 6
+
+    Output :    *   *   *   *   *   *
+                *   *               *
+                *       *           *
+                *           *       *
+                *               *   *  
+                *   *   *   *   *   *
+               
+*/
+
+class Pattern
+{
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   It is used to print given pattern on screen
+//  Input :         Integer
+//  Output :        void
+//  Auther :        Diksha Kadu Vispute
+//  Date :          26/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        if(i != j)
+        {
+            System.out.println("Invalid input");
+            System.out.println("number of rows and columns should be same");
+            return;
+        }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for( j = iCol; j >= 1; j--)
+            {                 
+                if((i == j) || (i == 1) || (i == iRow) || (j == 1) || (j == iCol))
+                {
+                    System.out.print("*\t");                 
+                }
+                else 
+                {
+                    System.out.print("\t");                 
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class program31_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter number the rows : ");
+        iValue1 =  sobj.nextInt();
+
+        System.out.println("Enter number the columns : ");
+        iValue2 =  sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
